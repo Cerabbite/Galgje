714502ef6288cc31bf3248d08e5bbd1697e54d68
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2023-09-15 11:07:57 +0200

    Project FINISHED

--- Galgjev3/Form1.Designer.cs
+++ Galgjev3/Form1.Designer.cs
@@ -32,7 +32,14 @@
             textBox1 = new TextBox();
             lbl_output = new Label();
             img_galgje = new PictureBox();
+            popup_panel = new Panel();
+            lbl_word_was_txt = new Label();
+            lbl_word_was = new Label();
+            btn_restart = new Button();
+            lbl_game_state = new Label();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)img_galgje).BeginInit();
+            popup_panel.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -76,11 +83,76 @@
             img_galgje.TabStop = false;
             img_galgje.Click += img_galgje_Click;
             // 
+            // popup_panel
+            // 
+            popup_panel.BackColor = SystemColors.ActiveBorder;
+            popup_panel.Controls.Add(lbl_word_was_txt);
+            popup_panel.Controls.Add(lbl_word_was);
+            popup_panel.Controls.Add(btn_restart);
+            popup_panel.Controls.Add(lbl_game_state);
+            popup_panel.Controls.Add(label2);
+            popup_panel.Location = new Point(147, 30);
+            popup_panel.Name = "popup_panel";
+            popup_panel.Size = new Size(489, 372);
+            popup_panel.TabIndex = 4;
+            popup_panel.Paint += game_over_panel_Paint;
+            // 
+            // lbl_word_was_txt
+            // 
+            lbl_word_was_txt.AutoSize = true;
+            lbl_word_was_txt.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_word_was_txt.Location = new Point(131, 136);
+            lbl_word_was_txt.Name = "lbl_word_was_txt";
+            lbl_word_was_txt.Size = new Size(109, 30);
+            lbl_word_was_txt.TabIndex = 4;
+            lbl_word_was_txt.Text = "Word was:";
+            lbl_word_was_txt.Click += lbl_word_was_txt_Click;
+            // 
+            // lbl_word_was
+            // 
+            lbl_word_was.AutoSize = true;
+            lbl_word_was.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_word_was.Location = new Point(246, 136);
+            lbl_word_was.Name = "lbl_word_was";
+            lbl_word_was.Size = new Size(68, 30);
+            lbl_word_was.TabIndex = 3;
+            lbl_word_was.Text = "label3";
+            lbl_word_was.Click += lbl_word_was_Click;
+            // 
+            // btn_restart
+            // 
+            btn_restart.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_restart.Location = new Point(161, 289);
+            btn_restart.Name = "btn_restart";
+            btn_restart.Size = new Size(195, 63);
+            btn_restart.TabIndex = 2;
+            btn_restart.Text = "Restart";
+            btn_restart.UseVisualStyleBackColor = true;
+            btn_restart.Click += btn_restart_Click;
+            // 
+            // lbl_game_state
+            // 
+            lbl_game_state.AutoSize = true;
+            lbl_game_state.Font = new Font("Segoe UI", 68.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_game_state.Location = new Point(-13, -3);
+            lbl_game_state.Name = "lbl_game_state";
+            lbl_game_state.Size = new Size(523, 120);
+            lbl_game_state.TabIndex = 1;
+            lbl_game_state.Text = "Game Over";
+            // 
+            // label2
+            // 
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 23);
+            label2.TabIndex = 0;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(popup_panel);
             Controls.Add(img_galgje);
             Controls.Add(lbl_output);
             Controls.Add(textBox1);
@@ -89,6 +161,8 @@
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)img_galgje).EndInit();
+            popup_panel.ResumeLayout(false);
+            popup_panel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -99,5 +173,11 @@
         private TextBox textBox1;
         private Label lbl_output;
         private PictureBox img_galgje;
+        private Panel popup_panel;
+        private Label label2;
+        private Label lbl_game_state;
+        private Button btn_restart;
+        private Label lbl_word_was;
+        private Label lbl_word_was_txt;
     }
 }
--- Galgjev3/Form1.cs
+++ Galgjev3/Form1.cs
@@ -10,7 +10,6 @@ namespace Galgjev3
         public Form1()
         {
             InitializeComponent();
-            // test();
         }
 
         public class var_class
@@ -18,9 +17,14 @@ namespace Galgjev3
             public static string random_word = "";
             public static string correct_chars = "";
             public static string correct_letters = "";
-            public static string typed_letters = "";
             public static string wrong_letters = "";
             public static string correct_output = "";
+            public static string typed_letters = "";
+
+            public static bool correct = false;
+            public static bool woord_geraden = false;
+            public static bool duplicate_input = false;
+            //public static List<char> typed_letters = new List<char>();
 
             public static int wrong_guesses = -1;
 
@@ -28,12 +32,19 @@ namespace Galgjev3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            // Disable game over and success components
+            popup_panel.Hide();
+            lbl_game_state.Visible = false;
+            btn_restart.Visible = false;
+            lbl_word_was_txt.Visible = false;
+            lbl_word_was.Visible = false;
+
             string[] cars = { "GYMZAAL", "CLUBPAS", "CUPCAKE", "PLOFKIP", "WIJNZAK", "WEBSHOP", "KETCHUP" };
+
             Random rnd = new Random();
             int indx = rnd.Next(0, cars.Length);
             var_class.random_word = cars[indx].ToLower();
-            Debug.WriteLine(var_class.random_word);
+
             for (int i = 0; i < var_class.random_word.Length; i++)
             {
                 var_class.correct_output += "-";
@@ -49,29 +60,33 @@ namespace Galgjev3
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             Debug.WriteLine("The START");
-            if (var_class.wrong_guesses >= 6)
+
+            if (is_game_over())
             {
                 Debug.WriteLine("Game Over");
-                //TODO: GAME OVER
+
+                lbl_word_was.Text = var_class.random_word;
+                lbl_game_state.Text = "Game Over";
+
+                popup_panel.Show();
+                lbl_game_state.Visible = true;
+                btn_restart.Visible = true;
+                lbl_word_was_txt.Visible = true;
+                lbl_word_was.Visible = true;
             }
 
             string userInput = textBox1.Text;
             Debug.WriteLine(userInput);
 
-            /*
-             * TODO: VALIDATE INPUT 
-            */
-
             textBox1.Text = "";
 
-            // TODO: Remove letters from userInput that are in typed_letters
-            var_class.typed_letters += userInput;
+            var_class.correct = false;
+
+            check_input(userInput);
 
             char[] random_word_array_chars = var_class.random_word.ToCharArray();
 
             int i = 0;
-            bool correct = false;
-            //int c = 0;
             foreach (char random_word_char in random_word_array_chars)
             {
                 if (!string.IsNullOrWhiteSpace(userInput))
@@ -79,8 +94,7 @@ namespace Galgjev3
                     // Check if current character is in random_word
                     if (Char.ToLower(userInput[0]) == random_word_char)
                     {
-                        correct = true;
-                        //c = 1;
+                        var_class.correct = true;
 
                         StringBuilder sb = new StringBuilder(var_class.correct_output);
                         sb[i] = userInput[0];
@@ -89,49 +103,83 @@ namespace Galgjev3
 
                         var_class.correct_letters += userInput;
                         Debug.WriteLine($"Check {var_class.correct_letters}");
-                        //TODO: correcte letters invullen op ___
-                        if (var_class.correct_letters.Length == var_class.random_word.Length)
+                        Debug.WriteLine($"Correct_output: {var_class.correct_output}");
+                        //if (var_class.correct_letters.Length == var_class.random_word.Length)
+                        if (is_correct_word())
                         {
+                            var_class.woord_geraden = true;
                             Debug.WriteLine("Woord is compleet");
-                            // TODO: POPUP "Gefeliciteerd \nOpnieuw?"
+                            // TODO: POPUP "Gefeliciteerd \n Opnieuw?"
                         }
                     }
                     else if (!Char.IsLetter(userInput[0]))
                     {
-                        correct = true;
+                        var_class.correct = true;
                     }
                 }
                 else
                 {
-                    correct = true;
+                    var_class.correct = true;
                 }
-                //
-                Debug.WriteLine($"[RANDOMWORD] = {var_class.random_word} - {userInput} | Substring: {random_word_char}");
 
                 i++;
             }
 
-            Debug.WriteLine($"Voor: {correct}");
-            Debug.WriteLine($"A: {var_class.wrong_guesses}");
 
-            if (correct == false && correct != true)
+            if (var_class.correct == false && var_class.duplicate_input == false)
             {
-                Debug.WriteLine($"Tijdens: {correct}");
-                var_class.wrong_guesses++;
-                Debug.WriteLine($"B: {var_class.wrong_guesses}");
-                this.img_galgje.ImageLocation = $"\\images\\{var_class.wrong_guesses}.png";
-
+                if (var_class.wrong_guesses < 6)
+                {
+                    var_class.wrong_guesses++;
+                }
+                
+                this.img_galgje.ImageLocation = $"D:\\GitHub\\School\\Fontys\\software\\Galgjev3\\Galgjev3\\images\\{var_class.wrong_guesses}.png";
                 var_class.wrong_letters += userInput;
                 this.lbl_output.Text = var_class.wrong_letters;
-            }
-            else if (correct == true) { Debug.WriteLine("gelukt"); }
 
-            Debug.WriteLine($"Na: {correct}");
-            Debug.WriteLine($"C: {var_class.wrong_guesses}");
-            Debug.WriteLine($"IMG SRC: {this.img_galgje.ImageLocation}");
+            }
+            else if (var_class.correct == true)
+            {
+               
+                Debug.WriteLine("gelukt");
+            }
+            
+            if (var_class.woord_geraden == true)
+            {
+                lbl_game_state.Text = "Success";
+
+                popup_panel.Show();
+                lbl_game_state.Visible = true;
+                btn_restart.Visible = true;
+                //lbl_word_was_txt.Visible = true;
+                //lbl_word_was.Visible = true;
+            }
+
+            if (var_class.duplicate_input == true)
+            {
+                this.lbl_output.Text = var_class.wrong_letters;
+            }
 
             this.label1.Text = var_class.correct_output;
-            //Thread.Sleep(500);
+        }
+
+        static bool is_game_over()
+        {
+            return (var_class.wrong_guesses > 5);
+        }
+
+        static bool is_correct_word()
+        {
+            if (var_class.correct_output == var_class.random_word)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void start_up()
+        {
+
         }
 
         private void lbl_output_Click(object sender, EventArgs e)
@@ -141,6 +189,77 @@ namespace Galgjev3
         private void img_galgje_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void game_over_panel_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void lbl_word_was_txt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_word_was_Click(object sender, EventArgs e)
+        {
+        }
+
+        static void check_input(string userInput)
+        {
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                Debug.WriteLine(var_class.typed_letters);
+                if (var_class.typed_letters.ToLower().Contains(userInput.ToLower()))
+                {
+                    Debug.WriteLine($"letter {userInput} al gebruikt");
+                    var_class.duplicate_input = true;
+                }
+                var_class.typed_letters += userInput;
+                Debug.WriteLine($"Gebruikte letters: {var_class.typed_letters}");
+            }
+            else { Debug.WriteLine("geen letter gebuirkt"); }
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            // Reset everything
+            var_class.random_word = "";
+            var_class.correct_chars = "";
+            var_class.correct_letters = "";
+            var_class.wrong_letters = "";
+            var_class.correct_output = "";
+            var_class.typed_letters = "";
+
+            var_class.correct = false;
+            var_class.woord_geraden = false;
+            var_class.duplicate_input = false;
+
+            var_class.woord_geraden = false;
+
+            var_class.correct_output = "";
+            this.img_galgje.ImageLocation = "D:\\GitHub\\School\\Fontys\\software\\Galgjev3\\Galgjev3\\images\\-1.png";
+            var_class.wrong_guesses = -1;
+            var_class.wrong_letters = "";
+
+            // Disable game over and success components
+            popup_panel.Hide();
+            lbl_game_state.Visible = false;
+            btn_restart.Visible = false;
+            lbl_word_was_txt.Visible = false;
+            lbl_word_was.Visible = false;
+
+            string[] cars = { "GYMZAAL", "CLUBPAS", "CUPCAKE", "PLOFKIP", "WIJNZAK", "WEBSHOP", "KETCHUP" };
+
+            Random rnd = new Random();
+            int indx = rnd.Next(0, cars.Length);
+            var_class.random_word = cars[indx].ToLower();
+
+            for (int i = 0; i < var_class.random_word.Length; i++)
+            {
+                var_class.correct_output += "-";
+            }
+            this.label1.Text = var_class.correct_output;
+            this.lbl_output.Text = "";
         }
     }
 
